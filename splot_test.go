7b0d88fb1c0505d67d06847a33b0cc032f90632b
Vikram Ramesh <vikram132@gmail.com>
2017-06-25 15:13:44 -0700

    Added tests for parseRow

--- splot_test.go
+++ splot_test.go
@@ -7,18 +7,11 @@ import (
 	"testing"
 )
 
-type TestLoadDataPair struct {
-	testData string
-	expected *DataSet
-}
-
-type TestLoadInvalidDataPair struct {
-	testData string
-	expected error
-}
-
-func TestLoadValidData(t *testing.T) {
-	testCases := []TestLoadDataPair{
+func TestLoadDataValid(t *testing.T) {
+	testCases := []struct {
+		testData string
+		expected *DataSet
+	}{
 		{"x,y", &DataSet{[]Point{}, "x", "y"}},
 		{"x,y\n", &DataSet{[]Point{}, "x", "y"}},
 		{"x,y,ignored", &DataSet{[]Point{}, "x", "y"}},
@@ -39,8 +32,11 @@ func TestLoadValidData(t *testing.T) {
 	}
 }
 
-func TestLoadInvalidData(t *testing.T) {
-	testCases := []TestLoadInvalidDataPair{
+func TestLoadDataInvalid(t *testing.T) {
+	testCases := []struct {
+		testData string
+		expected error
+	}{
 		{"", errors.New("No data found")},
 		{"x", errors.New("Header with 2 elements required")},
 		{"x,", errors.New("Header with 2 elements required")},
@@ -52,6 +48,53 @@ func TestLoadInvalidData(t *testing.T) {
 
 	for _, test := range testCases {
 		if _, err := loadData(strings.NewReader(test.testData)); err != nil {
+			if test.expected.Error() != err.Error() {
+				t.Errorf("Expected: %v\n Actual: %v", test.expected, err)
+			}
+		} else {
+			t.Errorf("Expected error, but none found!")
+		}
+	}
+}
+
+func TestParseRowValid(t *testing.T) {
+	testCases := []struct {
+		row      string
+		expected Point
+	}{
+		{"1,2", Point{1, 2}},
+		{"1123,2098", Point{1123, 2098}},
+	}
+
+	for _, test := range testCases {
+		if r, err := parseRow(test.row); err != nil {
+			t.Errorf("Expected: %v\n Actual Error: %v", test.expected, err)
+		} else {
+			if !(test.expected.x == r.x) || !(test.expected.y == r.y) {
+				t.Errorf("Expected: %v\n Actual: %v", test.expected, r)
+			}
+		}
+	}
+}
+
+func TestParseRowInvalid(t *testing.T) {
+	testCases := []struct {
+		row      string
+		expected error
+	}{
+		{"1", errors.New("coordinates require 2 values")},
+		{"foo", errors.New("coordinates require 2 values")},
+		{"foo,1", errors.New("foo is not a number")},
+		{"1,bar", errors.New("bar is not a number")},
+		{"foo,1,2", errors.New("foo is not a number")},
+		{"1,bar,2", errors.New("bar is not a number")},
+		{"foo,bar", errors.New("foo is not a number")},
+		{"1.0,2", errors.New("1.0 is not a number")},
+		{"1,2.0", errors.New("2.0 is not a number")},
+	}
+
+	for _, test := range testCases {
+		if _, err := parseRow(test.row); err != nil {
 			if test.expected.Error() != err.Error() {
 				t.Errorf("Expected: %v\n Actual: %v", test.expected, err)
 			}
