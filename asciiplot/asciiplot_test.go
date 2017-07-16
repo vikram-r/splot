44bed45f720fffb801091319264f4b588e20c9ee
Vikram Ramesh <vikram132@gmail.com>
2017-07-15 23:04:26 -0700

    Fixed broken tests

--- asciiplot/asciiplot_test.go
+++ asciiplot/asciiplot_test.go
@@ -17,8 +17,8 @@ func TestLoadDataValid(t *testing.T) {
 		{"x,y,ignored", &dataSet{[]point{}, "x", "y", 0, 0, 0, 0}},
 		{"x,y\n1,2", &dataSet{[]point{{1, 2}}, "x", "y", 1, 1, 2, 2}},
 		{"x,y\n1,2\n3,4", &dataSet{[]point{{1, 2}, {3, 4}}, "x", "y", 1, 3, 2, 4}},
-		{"x,y\n3,4\n1,2", &dataSet{[]point{{1, 2}, {3, 4}}, "x", "y", 1, 3, 2, 4}},
-		{"x,y\n3,4\n1,2\n2,8", &dataSet{[]point{{1, 2}, {2, 8}, {3, 4}}, "x", "y", 1, 3, 2, 8}},
+		{"x,y\n3,4\n1,2", &dataSet{[]point{{3, 4}, {1, 2}}, "x", "y", 1, 3, 2, 4}},
+		{"x,y\n3,4\n1,2\n2,8", &dataSet{[]point{{3, 4}, {1, 2}, {2, 8}}, "x", "y", 1, 3, 2, 8}},
 	}
 
 	for _, test := range testCases {
